3f44ae869c7657041add0bb93121db3d06d52e1d
Justdhif <noob1234five@gmail.com>
2025-06-27 20:06:29 +0700

    update: Origin from middleware

--- middlewares/corsConfig.js
+++ middlewares/corsConfig.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://portofolio-justdhif.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
